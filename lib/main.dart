63bd0f32ea3244cd1f3b2f4b8631d62e0012efc5
Steve Blythe <blythesr@gmail.com>
2022-08-05 13:59:22 +0100

    Initialise Firebase in Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:keep_track_toolkit/firebase_options.dart';
 import 'package:keep_track_toolkit/landing_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const KeepTrackToolkitApp());
 }
 
