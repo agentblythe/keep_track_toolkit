a816e1557f36378d0201d1cf0d5c381caac3d10a
Steve Blythe <blythesr@gmail.com>
2022-08-18 16:47:27 +0100

    Added tracker list widget

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:keep_track_toolkit/common-widgets/show_alert_dialog.dart';
 import 'package:keep_track_toolkit/profile/navigation/app_routes.dart';
 import 'package:keep_track_toolkit/profile/profile_manager.dart';
+import 'package:keep_track_toolkit/screens/home/tracker_list.dart';
 import 'package:keep_track_toolkit/services/auth.dart';
 import 'package:provider/provider.dart';
 
@@ -35,7 +36,7 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-      body: const Center(child: Text("Home Page")),
+      body: TrackerList(),
     );
   }
 
---
+++ lib/screens/home/tracker_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TrackerList extends StatelessWidget {
+  const TrackerList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("TrackerList"));
+  }
+}
